d339c5146af8b236a8384fc3d2b53e7badd5cedf
Marc Sibson <sibson@gmail.com>
2011-05-19 16:45:12 -0700

    setup: bump version for 0.2dev

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = open('README.rst', 'rt').read()
 
 setup(
     name='vncdotool',
-    version='0.1.1',
+    version='0.2.0dev',
     description='Command line VNC client',
     install_requires=[
         'Twisted',
