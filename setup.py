f1f450de5d56266d8872f26901a89e19290d1c3a
Marc Sibson <sibson@gmail.com>
2012-12-22 09:17:18 -0800

    set version 0.3.0, for release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = open('README.rst', 'rt').read()
 
 setup(
     name='vncdotool',
-    version='0.3.0.dev0',
+    version='0.3.0',
     description='Command line VNC client',
     install_requires=[
         'Twisted',
