d550a573ee95f37138aa63a29c3daf9c7ddc97d6
Marc Sibson <sibson@gmail.com>
2016-01-24 20:27:15 -0800

    drop offical 2.6 support, it'll probably work for a while still

--- setup.py
+++ setup.py
@@ -41,7 +41,6 @@ setup(
           'Operating System :: Microsoft :: Windows',
           'Operating System :: POSIX',
           'Programming Language :: Python',
-          'Programming Language :: Python :: 2.6',
           'Programming Language :: Python :: 2.7',
           'Topic :: Multimedia :: Graphics :: Viewers',
           'Topic :: Software Development :: Testing',
