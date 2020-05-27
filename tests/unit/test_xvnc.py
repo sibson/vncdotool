a98efccdeb6d3af708de7ac19f2583c8d0073b50
ponty <ponty@home>
2020-05-27 22:01:49 +0200

    test wth xvnc

---
+++ tests/unit/test_xvnc.py
@@ -0,0 +1,23 @@
+from vncdotool import api
+
+from pyvirtualdisplay import Display
+import tempfile
+
+
+def test_color_xvnc():
+    with tempfile.NamedTemporaryFile(prefix="vnc_", suffix=".png") as png:
+
+        with Display(backend="xvnc", rfbport=5900 + 9876):
+            with api.connect("localhost:9876") as client:
+                client.timeout = 1
+                client.captureScreen(png.name)
+
+        with tempfile.NamedTemporaryFile(prefix="passwd_", delete=False) as passwd_file:
+            password = "123456"
+            vncpasswd_generated = b"\x49\x40\x15\xf9\xa3\x5e\x8b\x22"
+            passwd_file.write(vncpasswd_generated)
+            passwd_file.close()
+            with Display(backend="xvnc", rfbport=5900 + 1234, rfbauth=passwd_file.name):
+                with api.connect("localhost:1234", password=password) as client:
+                    client.timeout = 1
+                    client.captureScreen(png.name)
