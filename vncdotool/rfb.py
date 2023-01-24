sibson/vncdotool

commit 6c7374ccda910975231150c842da32d6a4e3ebbe

feat: Added support for QEMU Client Messages
The RFB protocol works well for remote access to X11 applications, but
is also used by Qemu to connect to virtual machines. There the
high-level KEYSYM must be translated back to a low-level KEYCODE as VMs
get hardware events. While a keyboard layout can be be configured
with Qemu, this is imperfect as there is no unique mapping from KEYSYM
to KEYCODE.

Thus Qemu added the "Extended Key Event Message" to include they
KEYCODE, which is already provided by the X11-server running on the
client. This makes configuring they keyboard layout for Qemu obsolete.

Closes #153

vncdotool/rfb.py
@@ -378,6 +378,7 @@ class RFBClient(Protocol):  # type: ignore[misc]
         Encoding.ZRLE,
         Encoding.PSEUDO_CURSOR,
         Encoding.PSEUDO_DESKTOP_SIZE,
+        Encoding.PSEUDO_QEMU_EXTENDED_KEY_EVENT,
     }
 
     def __init__(self) -> None:
@@ -387,6 +388,9 @@ class RFBClient(Protocol):  # type: ignore[misc]
         self._version: Ver = (0, 0)
         self._version_server: Ver = (0, 0)
         self._zlib_stream = zlib.decompressobj(0)
+        self.negotiated_encodings = {
+            Encoding.RAW,
+        }
 
     #------------------------------------------------------
     # states used on connection startup
@@ -614,6 +618,9 @@ class RFBClient(Protocol):  # type: ignore[misc]
                 self.expect(self._handleDecodePsuedoCursor, length, x, y, width, height)
             elif encoding == Encoding.PSEUDO_DESKTOP_SIZE:
                 self._handleDecodeDesktopSize(width, height)
+            elif encoding == Encoding.PSEUDO_QEMU_EXTENDED_KEY_EVENT:
+                self.negotiated_encodings.add(Encoding.PSEUDO_QEMU_EXTENDED_KEY_EVENT)
+                self.expect(self._handleConnection, 1)
             else:
                 log.msg(f"unknown encoding received (encoding {encoding})")
                 self._doConnection()

tests/unit/test_client.py
@@ -27,7 +27,9 @@ class TestVNCDoToolClient(TestCase):
         self.client.setEncodings.assert_called_once_with([
             client.rfb.Encoding.RAW,
             client.rfb.Encoding.PSEUDO_CURSOR,
-            client.rfb.Encoding.PSEUDO_DESKTOP_SIZE])
+            client.rfb.Encoding.PSEUDO_DESKTOP_SIZE,
+            client.rfb.Encoding.PSEUDO_QEMU_EXTENDED_KEY_EVENT,
+        ])
 
     def test_keyPress_single_alpha(self):
         cli = self.client

vncdotool/client.py
@@ -395,6 +395,8 @@ class VNCDoToolClient(rfb.RFBClient):
             encodings.append(rfb.Encoding.PSEUDO_CURSOR)
         if self.factory.pseudodesktop:
             encodings.append(rfb.Encoding.PSEUDO_DESKTOP_SIZE)
+        if self.factory.qemu_extended_key:
+            encodings.append(rfb.Encoding.PSEUDO_QEMU_EXTENDED_KEY_EVENT)
         self.setEncodings(encodings)
         self.factory.clientConnectionMade(self)
 
@@ -504,6 +506,7 @@ class VNCDoToolFactory(rfb.RFBFactory):
     pseudocursor = False
     nocursor = False
     pseudodesktop = True
+    qemu_extended_key = True
     force_caps = False
 
     def __init__(self) -> None:

vncdotool/loggingproxy.py
@@ -58,12 +58,19 @@ class MsgC2S(IntEnum):
     QEMU_CLIENT_MESSAGE = 255
 
 
+class QemuClientMessage(IntEnum):
+    """https://github.com/rfbproto/rfbproto/blob/master/rfbproto.rst#qemu-client-message"""
+    EXTENDED_KEY_EVENT = 0
+    AUDIO = 1
+
+
 TYPE_LEN = {
     MsgC2S.SET_PIXEL_FORMAT: 20,
     MsgC2S.SET_ENCODING: 4,
     MsgC2S.FRAMEBUFFER_UPDATE_REQUEST: 10,
     MsgC2S.KEY_EVENT: 8,
     MsgC2S.POINTER_EVENT: 6,
+    MsgC2S.QEMU_CLIENT_MESSAGE: 1,
 }
 
 REVERSE_MAP = {v: n for (n, v) in KEYMAP.items()}
@@ -146,8 +153,20 @@ class RFBServer(Protocol):  # type: ignore[misc]
             self.handle_pointerEvent(x, y, buttonmask)
         elif ptype == MsgC2S.CLIENT_CUT_TEXT:
             self.handle_clientCutText(block)
+        elif ptype == MsgC2S.QEMU_CLIENT_MESSAGE:
+            subtype, = unpack('!B', block)
+            if subtype == QemuClientMessage.EXTENDED_KEY_EVENT:
+                self._handler = self._handle_qemuExtendedKeyEvent, 10
+            else:
+                raise ProtocolError(subtype)
         else:
             raise ProtocolError(ptype)
+
+    def _handle_qemuExtendedKeyEvent(self) -> None:
+        down_flag, keysym, keycode = unpack_from("!HII", self.buffer)
+        del self.buffer[:12]
+        self.handle_keyEventExtended(keysym, down_flag, keycode)
+        self._handler = self._handle_protocol, 1
 
     def handle_setPixelFormat(self, bbp: int, depth: int, bigendian: bool, truecolor: bool, rmax: int, gmax: int, bmax: int, rshift: int, gshift: int, bshift: int) -> None:
         pass
@@ -166,6 +185,9 @@ class RFBServer(Protocol):  # type: ignore[misc]
 
     def handle_clientCutText(self, block: bytes) -> None:
         pass
+
+    def handle_keyEventExtended(self, keysym: int, down: bool, keycode: int) -> None:
+        self.handle_keyEvent(keysym, down)
 
 
 class NullTransport:
@@ -294,6 +316,7 @@ class VNCLoggingServerFactory(portforward.ProxyFactory):  # type: ignore[misc]
     pseudocursor = False
     nocursor = False
     pseudodesktop = True
+    qemu_extended_key = True
     force_caps = False
 
     password_required = False

commit 53627d814f97c5d14fd2cc9b879c4b967058b4cc

fix(qemu): Fix Qemu extended key handling
The PSEUDO_QEMU_EXTENDED_KEY_EVENT encoding may be part of a larger
set of update rectangles. Continue parsing the following rectangles
instead of going back to connection handling.

Fixes: 6c7374ccda910975231150c842da32d6a4e3ebbe

vncdotool/rfb.py
@@ -620,7 +620,6 @@ class RFBClient(Protocol):  # type: ignore[misc]
                 self._handleDecodeDesktopSize(width, height)
             elif encoding == Encoding.PSEUDO_QEMU_EXTENDED_KEY_EVENT:
                 self.negotiated_encodings.add(Encoding.PSEUDO_QEMU_EXTENDED_KEY_EVENT)
-                self.expect(self._handleConnection, 1)
             else:
                 log.msg(f"unknown encoding received (encoding {encoding})")
                 self._doConnection()