sibson/vncdotool

commit 3dacd57f1d2dbd1bb5009a2429a9a582ab5136c5

Improve logging and debugging (#242)
* refactor: Use getLogger(__name__)

instead of using hard-coded names.

* feat: Log unknown message types

vncdotool/loggingproxy.py
@@ -3,7 +3,6 @@ import os.path
 import socket
 import sys
 import time
-from enum import IntEnum
 from struct import unpack, unpack_from
 from typing import IO, Callable, List, Optional, Sequence, Tuple, Union
 
@@ -12,16 +11,16 @@ from twisted.protocols import portforward
 from twisted.python.failure import Failure
 
 from .client import KEYMAP, VNCDoToolClient
-from .rfb import Rect
+from .rfb import AuthTypes, IntEnumLookup, Rect
 
-log = logging.getLogger('proxy')
+log = logging.getLogger(__name__)
 
 
 class ProtocolError(Exception):
     """ VNC Protocol error """
 
 
-class MsgC2S(IntEnum):
+class MsgC2S(IntEnumLookup):
     """RFC 6143 §7.5. Client-to-Server Messages."""
     SET_PIXEL_FORMAT = 0
     SET_ENCODING = 2
@@ -58,7 +57,7 @@ class MsgC2S(IntEnum):
     QEMU_CLIENT_MESSAGE = 255
 
 
-class QemuClientMessage(IntEnum):
+class QemuClientMessage(IntEnumLookup):
     """https://github.com/rfbproto/rfbproto/blob/master/rfbproto.rst#qemu-client-message"""
     EXTENDED_KEY_EVENT = 0
     AUDIO = 1
@@ -109,7 +108,8 @@ class RFBServer(Protocol):  # type: ignore[misc]
             self._handler = self._handle_security, 1
 
     def _handle_security(self) -> None:
-        # sectype = self.buffer[0]
+        sectype = self.buffer[0]
+        log.debug("Client selected %r", AuthTypes.lookup(sectype))
         del self.buffer[:1]
         self._handler = self._handle_clientInit, 1
 
@@ -118,7 +118,8 @@ class RFBServer(Protocol):  # type: ignore[misc]
         self._handler = self._handle_clientInit, 1
 
     def _handle_clientInit(self) -> None:
-        # shared = self.buffer[0]
+        shared = self.buffer[0]
+        log.debug("Shared server: %s", shared)
         del self.buffer[:1]
         # XXX react to shared
         # XXX send serverInit
@@ -158,8 +159,10 @@ class RFBServer(Protocol):  # type: ignore[misc]
             if subtype == QemuClientMessage.EXTENDED_KEY_EVENT:
                 self._handler = self._handle_qemuExtendedKeyEvent, 10
             else:
+                log.debug("Unhandled subtype %r", QemuClientMessage.lookup(subtype))
                 raise ProtocolError(subtype)
         else:
+            log.debug("Unhandled response %r", MsgC2S.lookup(ptype))
             raise ProtocolError(ptype)
 
     def _handle_qemuExtendedKeyEvent(self) -> None:

vncdotool/api.py
@@ -24,7 +24,7 @@ TProxy = TypeVar("TProxy", bound="ThreadedVNCClientProxy")
 
 __all__ = ['connect']
 
-log = logging.getLogger('vncdotool.api')
+log = logging.getLogger(__name__)
 
 _THREAD: Optional[threading.Thread] = None
 

vncdotool/client.py
@@ -23,7 +23,7 @@ from . import rfb
 
 TClient = TypeVar("TClient", bound="VNCDoToolClient")
 
-log = logging.getLogger('vncdotool.client')
+log = logging.getLogger(__name__)
 
 
 KEYMAP = {

vncdotool/rfb.py
@@ -40,7 +40,13 @@ Ver = Tuple[int, int]
 #~ from twisted.internet import reactor
 
 
-class Encoding(IntEnum):
+class IntEnumLookup(IntEnum):
+    @classmethod
+    def lookup(cls, value: int) -> object:
+        return cls._value2member_map_.get(value, f"<{cls.__name__}.UNKNOWN: {value:x}>")
+
+
+class Encoding(IntEnumLookup):
     """encoding-type for SetEncodings()"""
 
     def __new__(cls, value: int) -> "Encoding":
@@ -171,7 +177,7 @@ class HextileEncoding(IntFlag):
     SUBRECTS_COLORED = 16
 
 
-class AuthTypes(IntEnum):
+class AuthTypes(IntEnumLookup):
     """RFC 6143 §7.1.2. Security Handshake."""
     INVALID = 0
     NONE = 1
@@ -215,7 +221,7 @@ class AuthTypes(IntEnum):
     REALVNC_192 = 192
 
 
-class MsgS2C(IntEnum):
+class MsgS2C(IntEnumLookup):
     """RFC 6143 §7.6. Server-to-Client Messages."""
     FRAMEBUFFER_UPDATE = 0
     SET_COLUOR_MAP_ENTRIES = 1
@@ -437,6 +443,8 @@ class RFBClient(Protocol):  # type: ignore[misc]
 
     def _handleSecurityTypes(self, block: bytes) -> None:
         types = unpack(f"!{len(block)}B", block)
+        for sec_type in types:
+            log.msg(f"Offered {AuthTypes.lookup(sec_type)!r}")
         valid_types = set(types) & self.SUPPORTED_AUTHS
         if valid_types:
             sec_type = max(valid_types)
@@ -464,7 +472,7 @@ class RFBClient(Protocol):  # type: ignore[misc]
         elif auth == AuthTypes.VNC_AUTHENTICATION:
             self.expect(self._handleVNCAuth, 16)
         else:
-            log.msg(f"unknown auth response ({auth})")
+            log.msg(f"unknown auth response {AuthTypes.lookup(auth)!r}")
 
     def _handleConnFailed(self, block: bytes) -> None:
         (waitfor,) = unpack("!I", block)
@@ -588,7 +596,7 @@ class RFBClient(Protocol):  # type: ignore[misc]
         elif msgid == MsgS2C.SERVER_CUT_TEXT:
             self.expect(self._handleServerCutText, 7)
         else:
-            log.msg(f"unknown message received (id {msgid})")
+            log.msg(f"unknown message received {MsgS2C.lookup(msgid)!r}")
             self.transport.loseConnection()
 
     def _handleFramebufferUpdate(self, block: bytes) -> None:
@@ -633,7 +641,7 @@ class RFBClient(Protocol):  # type: ignore[misc]
             elif encoding == Encoding.PSEUDO_QEMU_EXTENDED_KEY_EVENT:
                 self.negotiated_encodings.add(Encoding.PSEUDO_QEMU_EXTENDED_KEY_EVENT)
             else:
-                log.msg(f"unknown encoding received (encoding {encoding})")
+                log.msg(f"unknown encoding received {Encoding.lookup(encoding)!r}")
                 self.transport.loseConnection()
         else:
             self._doConnection()
@@ -1101,6 +1109,7 @@ class RFBClient(Protocol):  # type: ignore[misc]
     def setEncodings(self, list_of_encodings: Collection[Encoding]) -> None:
         self.transport.write(pack("!BxH", 2, len(list_of_encodings)))
         for encoding in list_of_encodings:
+            log.msg(f"Offering {encoding!r}")
             self.transport.write(pack("!i", encoding))
 
     def framebufferUpdateRequest(

commit 297d2df5e2d44eb16666709ff2aceb90c5d1dbb6

fix(proxy): More logging
Fixes: 3dacd57f1d2dbd1bb5009a2429a9a582ab5136c5

vncdotool/loggingproxy.py
@@ -11,7 +11,7 @@ from twisted.protocols import portforward
 from twisted.python.failure import Failure
 
 from .client import KEYMAP, VNCDoToolClient
-from .rfb import AuthTypes, IntEnumLookup, PixelFormat, Rect
+from .rfb import AuthTypes, Encoding, IntEnumLookup, PixelFormat, Rect
 
 log = logging.getLogger(__name__)
 
@@ -119,7 +119,7 @@ class RFBServer(Protocol):  # type: ignore[misc]
 
     def _handle_clientInit(self) -> None:
         shared = self.buffer[0]
-        log.debug("Shared server: %s", shared)
+        log.debug("Client shares: %s", shared)
         del self.buffer[:1]
         # XXX react to shared
         # XXX send serverInit
@@ -143,6 +143,8 @@ class RFBServer(Protocol):  # type: ignore[misc]
             nbytes = 4 * nencodings
             encodings = unpack_from('!' + 'I' * nencodings, self.buffer)
             del self.buffer[:nbytes]
+            for encoding in encodings:
+                log.debug(f"Client announces {Encoding.lookup(encoding)!r}")
             self.handle_setEncodings(encodings)
         elif ptype == MsgC2S.FRAMEBUFFER_UPDATE_REQUEST:
             inc, x, y, w, h = unpack('!BHHHH', block)