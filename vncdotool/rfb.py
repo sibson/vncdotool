sibson/vncdotool

commit 53627d814f97c5d14fd2cc9b879c4b967058b4cc

fix(qemu): Fix Qemu extended key handling
The PSEUDO_QEMU_EXTENDED_KEY_EVENT encoding may be part of a larger
set of update rectangles. Continue parsing the following rectangles
instead of going back to connection handling.

Fixes: 6c7374ccda910975231150c842da32d6a4e3ebbe

vncdotool/rfb.py
@@ -620,7 +620,6 @@ class RFBClient(Protocol):  # type: ignore[misc]
                 self._handleDecodeDesktopSize(width, height)
             elif encoding == Encoding.PSEUDO_QEMU_EXTENDED_KEY_EVENT:
                 self.negotiated_encodings.add(Encoding.PSEUDO_QEMU_EXTENDED_KEY_EVENT)
-                self.expect(self._handleConnection, 1)
             else:
                 log.msg(f"unknown encoding received (encoding {encoding})")
                 self._doConnection()

commit a9a62c931481a0efd00d7b77a3ef894a69a6c030

fix(qemu): Fix Qemu extended key handling again
The PSEUDO_QEMU_EXTENDED_KEY_EVENT encoding may be part of a larger
set of update rectangles. Continue parsing the following rectangles
instead of going back to connection handling.

Actually call `_doConnection()` to handle either the next rectangle in
row or return to `_handleConnection` if it was the last rectangle.

Only call callback `commitUpdate()` if at least one real update
happened. Otherwise a PSEUDO_QEMU_EXTENDED_KEY_EVENT will trigger the
Deferred and `capture` will fail, as no real update has happened.

Fixes: 53627d814f97c5d14fd2cc9b879c4b967058b4cc

vncdotool/rfb.py
@@ -659,7 +659,8 @@ class RFBClient(Protocol):  # type: ignore[misc]
         if self.rectangles:
             self.expect(self._handleRectangle, 12)
         else:
-            self.commitUpdate(self.rectanglePos)
+            if self.rectanglePos:
+                self.commitUpdate(self.rectanglePos)
             self.expect(self._handleConnection, 1)
 
     def _handleRectangle(self, block: bytes) -> None:
@@ -691,6 +692,8 @@ class RFBClient(Protocol):  # type: ignore[misc]
                 self._handleDecodeDesktopSize(width, height)
             elif encoding == Encoding.PSEUDO_QEMU_EXTENDED_KEY_EVENT:
                 self.negotiated_encodings.add(Encoding.PSEUDO_QEMU_EXTENDED_KEY_EVENT)
+                del self.rectanglePos[-1]  # undo append as this is no real update
+                self._doConnection()
             else:
                 log.msg(f"unknown encoding received {Encoding.lookup(encoding)!r}")
                 self.transport.loseConnection()