sibson/vncdotool

commit 15d1f70115ef4b30a3f089149d8812ee8eb2ce36

Improve Pixelformat handling (#243)
* fix(rfb): Encoding.lookup() for signed32

Encodings are defined using both (un-)signed-int32. Internally they are
converted to s32. Do the same when doing the lookup.

Fixes: 3dacd57f1d2dbd1bb5009a2429a9a582ab5136c5

* fix(client): PSEUDO_COLOR image format

The "Cursor Pseudo-encoding" uses the same pixel format as the regular
FB_UPDATE. The previous code assumed the hard-coded 32-bit RGBX format,
which lead to `Image.frombytes()` expecting more data than available if
only 24 bits-per-pixel are used.

Test setup:
  /tmp/vncdo/libvncserver-LibVNCServer-0.9.14/examples/pnmshow24 /usr/lib/x86_64-linux-gnu/perl5/5.32/Tk/demos/images/teapot.ppm
  vnclog -vvv -s localhost:0 /tmp/log
  gvncviewer localhost:2

> Traceback (most recent call last):
>   File "twisted/python/log.py", line 96, in callWithLogger
>     return callWithContext({"system": lp}, func, *args, **kw)
>   File "twisted/python/log.py", line 80, in callWithContext
>     return context.call({ILogContext: newCtx}, func, *args, **kw)
>   File "twisted/python/context.py", line 117, in callWithContext
>     return self.currentContext().callWithContext(ctx, func, *args, **kw)
>   File "twisted/python/context.py", line 82, in callWithContext
>     return func(*args, **kw)
> --- <exception caught here> ---
>   File "twisted/internet/posixbase.py", line 487, in _doReadOrWrite
>     why = selectable.doRead()
>   File "twisted/internet/tcp.py", line 248, in doRead
>     return self._dataReceived(data)
>   File "twisted/internet/tcp.py", line 253, in _dataReceived
>     rval = self.protocol.dataReceived(data)
>   File "vncdotool/loggingproxy.py", line 245, in dataReceived
>     self.vnclog.dataReceived(data)
>   File "vncdotool/rfb.py", line 1108, in dataReceived
>     self._handler()
>   File "vncdotool/rfb.py", line 1117, in _handleExpected
>     self._expected_handler(block, *self._expected_args, **self._expected_kwargs)

vncdotool/client.py
@@ -134,13 +134,25 @@ class AuthenticationError(Exception):
     """ VNC Server requires Authentication """
 
 
+RGB32 = rfb.PixelFormat(32, 24, False, True, 255, 255, 255, 0, 8, 16)
+RGB24 = rfb.PixelFormat(24, 24, False, True, 255, 255, 255, 0, 8, 16)
+BGR16 = rfb.PixelFormat(16, 16, False, True, 31, 63, 31, 11, 5, 0)
+PF2IM = {
+    RGB24: "RGB",
+    RGB32: "RGBX",
+    BGR16: "BGR;16",
+    rfb.PixelFormat(24, 24, False, True, 255, 255, 255, 16, 8, 0): "BGR;24",
+    rfb.PixelFormat(32, 24, False, True, 255, 255, 255, 16, 8, 0): "BGR;32",
+}
+
+
 class VNCDoToolClient(rfb.RFBClient):
     encoding = rfb.Encoding.RAW
     x = 0
     y = 0
     buttons = 0
     screen: Optional[Image.Image] = None
-    image_mode = "RGBX"
+    image_mode = PF2IM[rfb.PixelFormat()]
     deferred: Optional[Deferred] = None
 
     cursor: Optional[Image.Image] = None
@@ -356,32 +368,18 @@ class VNCDoToolClient(rfb.RFBClient):
         return self
 
     def setImageMode(self) -> None:
-        """ Extracts color ordering and 24 vs. 32 bpp info out of the pixel format information
+        """ Check support for PixelFormats announced by server or select client supported alternative.
         """
-        if self._version_server == (3, 889):
-            self.setPixelFormat(
-                bpp=16,
-                depth=16,
-                bigendian=False,
-                truecolor=True,
-                redmax=31,
-                greenmax=63,
-                bluemax=31,
-                redshift=11,
-                greenshift=5,
-                blueshift=0,
-            )
-            self.image_mode = "BGR;16"
-        elif (self.truecolor and (not self.bigendian) and self.depth == 24
-                and self.redmax == self.greenmax == self.bluemax == 255):
+        try:
+            self.image_mode = PF2IM[self.pixel_format]
+        except LookupError:
+            if self._version_server == (3, 889):  # Apple Remote Desktop
+                pixel_format = BGR16
+            else:
+                pixel_format = RGB32
 
-            pixel = ["X"] * self.bypp
-            offsets = [offset // 8 for offset in (self.redshift, self.greenshift, self.blueshift)]
-            for offset, color in zip(offsets, "RGB"):
-                pixel[offset] = color
-            self.image_mode = "".join(pixel)
-        else:
-            self.setPixelFormat()
+            self.setPixelFormat(pixel_format)
+            self.image_mode = PF2IM[pixel_format]
 
     #
     # base customizations
@@ -453,7 +451,7 @@ class VNCDoToolClient(rfb.RFBClient):
         if not width or not height:
             self.cursor = None
 
-        self.cursor = Image.frombytes('RGBX', (width, height), image)
+        self.cursor = Image.frombytes('RGB', (width, height), image, "raw", self.image_mode)
         self.cmask = Image.frombytes('1', (width, height), mask)
         self.cfocus = x, y
         self.drawCursor()

vncdotool/rfb.py
@@ -13,14 +13,14 @@ MIT License
 """
 
 import getpass
-import math
 import os
 import re
 import sys
 import zlib
+from dataclasses import astuple, dataclass
 from enum import IntEnum, IntFlag
-from struct import pack, unpack
-from typing import Any, Callable, Collection, Iterator, List, Optional, Tuple
+from struct import Struct, pack, unpack
+from typing import Any, Callable, ClassVar, Collection, Iterator, List, Optional, Tuple, cast
 
 from Crypto.Cipher import AES
 from Crypto.Hash import MD5
@@ -49,11 +49,16 @@ class IntEnumLookup(IntEnum):
 class Encoding(IntEnumLookup):
     """encoding-type for SetEncodings()"""
 
+    @staticmethod
+    def s32(value: int) -> int:
+        return value - 0x1_0000_0000 if value >= 0x8000_0000 else value
+
     def __new__(cls, value: int) -> "Encoding":
-        # Convert to signed32
-        if value >= 0x8000_0000:
-            value = value - 0x1_0000_0000
-        return int.__new__(cls, value)
+        return int.__new__(cls, cls.s32(value))
+
+    @classmethod
+    def lookup(cls, value: int) -> object:
+        return super().lookup(cls.s32(value))
 
     RAW = 0
     COPY_RECTANGLE = 1
@@ -324,6 +329,50 @@ KEY_BackSlash = 0x005C
 KEY_SpaceBar = 0x0020
 
 
+@dataclass(frozen=True)
+class PixelFormat:
+    """RFC 6143 §7.4. Pixel Format Data Structure"""
+
+    bpp: int = 32  # u8: bits-per-pixel
+    depth: int = 24  # u8
+    bigendian: bool = False  # u8
+    truecolor: bool = True  # u8
+    redmax: int = 255  # u16
+    greenmax: int = 255  # u16
+    bluemax: int = 255  # u16
+    redshift: int = 0  # u8
+    greenshift: int = 8  # u8
+    blueshift: int = 16  # u8
+
+    STRUCT: ClassVar = Struct("!BB??HHHBBBxxx")
+    VALIDATE: ClassVar = False
+
+    def __post_init__(self) -> None:
+        if not self.VALIDATE:
+            return
+        assert self.bpp in {8, 16, 24, 32}, f"bpp={self.bpp}"
+        assert 1 <= self.depth <= self.bpp, f"depth={self.depth} <= bpp={self.bpp}"
+        if self.truecolor:
+            for max, shift in zip(
+                (self.redmax, self.greenmax, self.bluemax),
+                (self.redshift, self.greenshift, self.blueshift),
+            ):
+                assert 1 <= max <= 0xffff, f"1 <= max={max} <= 0xffff"
+                assert max & (max + 1) == 0, f"max={max} not a 2**n-1"
+                assert 0 <= shift <= self.bpp - max.bit_length(), f"shift={shift} not in bpp={self.bpp}"
+
+    @property
+    def bypp(self) -> int:  # bytes-per-pixel
+        return (7 + self.bpp) // 8
+
+    @classmethod
+    def from_bytes(cls, block: bytes) -> "PixelFormat":
+        return cls(*cls.STRUCT.unpack(block))
+
+    def to_bytes(self) -> bytes:
+        return cast(bytes, self.STRUCT.pack(*astuple(self)))
+
+
 # ZRLE helpers
 def _zrle_next_bit(it: Iterator[int], pixels_in_tile: int) -> Iterator[int]:
     num_pixels = 0
@@ -406,6 +455,11 @@ class RFBClient(Protocol):  # type: ignore[misc]
         self.negotiated_encodings = {
             Encoding.RAW,
         }
+        self.pixel_format = PixelFormat()
+
+    @property
+    def bypp(self) -> int:
+        return self.pixel_format.bypp
 
     # ------------------------------------------------------
     # states used on connection startup
@@ -569,12 +623,8 @@ class RFBClient(Protocol):  # type: ignore[misc]
 
     def _handleServerInit(self, block: bytes) -> None:
         (self.width, self.height, pixformat, namelen) = unpack("!HH16sI", block)
-        (
-            self.bpp, self.depth, self.bigendian, self.truecolor,
-            self.redmax, self.greenmax, self.bluemax,
-            self.redshift, self.greenshift, self.blueshift,
-        ) = unpack("!BBBBHHHBBBxxx", pixformat)
-        self.bypp = self.bpp // 8  # calc bytes per pixel
+        self.pixel_format = PixelFormat.from_bytes(pixformat)
+        log.msg(f"Native {self.pixel_format} bytes={self.pixel_format.bypp}")
         self.expect(self._handleServerName, namelen)
 
     def _handleServerName(self, block: bytes) -> None:
@@ -614,6 +664,7 @@ class RFBClient(Protocol):  # type: ignore[misc]
 
     def _handleRectangle(self, block: bytes) -> None:
         (x, y, width, height, encoding) = unpack("!HHHHi", block)
+        log.msg(f"x={x} y={y} w={width} h={height} {Encoding.lookup(encoding)!r}")
         if encoding == Encoding.PSEUDO_LAST_RECT:
             self.rectangles = 0
 
@@ -634,7 +685,7 @@ class RFBClient(Protocol):  # type: ignore[misc]
                 self.expect(self._handleDecodeZRLE, 4, x, y, width, height)
             elif encoding == Encoding.PSEUDO_CURSOR:
                 length = width * height * self.bypp
-                length += int(math.floor((width + 7.0) / 8)) * height
+                length += ((width + 7) // 8) * height
                 self.expect(self._handleDecodePsuedoCursor, length, x, y, width, height)
             elif encoding == Encoding.PSEUDO_DESKTOP_SIZE:
                 self._handleDecodeDesktopSize(width, height)
@@ -1084,27 +1135,10 @@ class RFBClient(Protocol):  # type: ignore[misc]
     # client -> server messages
     # ------------------------------------------------------
 
-    def setPixelFormat(
-        self,
-        bpp: int = 32,
-        depth: int = 24,
-        bigendian: bool = False,
-        truecolor: bool = True,
-        redmax: int = 255,
-        greenmax: int = 255,
-        bluemax: int = 255,
-        redshift: int = 0,
-        greenshift: int = 8,
-        blueshift: int = 16
-    ) -> None:
-        pixformat = pack("!BBBBHHHBBBxxx", bpp, depth, bigendian, truecolor, redmax, greenmax, bluemax, redshift, greenshift, blueshift)
+    def setPixelFormat(self, pixel_format: PixelFormat) -> None:
+        pixformat = pixel_format.to_bytes()
         self.transport.write(pack("!Bxxx16s", 0, pixformat))
-        # rember these settings
-        self.bpp, self.depth, self.bigendian, self.truecolor = bpp, depth, bigendian, truecolor
-        self.redmax, self.greenmax, self.bluemax = redmax, greenmax, bluemax
-        self.redshift, self.greenshift, self.blueshift = redshift, greenshift, blueshift
-        self.bypp = self.bpp // 8  # calc bytes per pixel
-        #~ print(self.bypp)
+        self.pixel_format = pixel_format
 
     def setEncodings(self, list_of_encodings: Collection[Encoding]) -> None:
         self.transport.write(pack("!BxH", 2, len(list_of_encodings)))
@@ -1240,7 +1274,7 @@ if __name__ == '__main__':
     class RFBTest(RFBClient):
         """dummy client"""
         def vncConnectionMade(self) -> None:
-            print(f"Screen format: depth={self.depth} bytes_per_pixel={self.bpp}")
+            print(f"Screen format: {self.pixel_format}")
             print(f"Desktop name: {self.name!r}")
             self.SetEncodings([Encoding.RAW])
             self.FramebufferUpdateRequest()

tests/unit/test_client.py
@@ -5,6 +5,14 @@ from vncdotool import client, rfb
 
 class TestVNCDoToolClient(TestCase):
 
+    MSG_HANDSHAKE = b"RFB 003.003\n"
+    MSG_INIT = (
+        b"\x00\x00"  # width
+        b"\x00\x00"  # height
+        b"\x20\x18\x00\x01\x00\xff\x00\xff\x00\xff\x00\x08\x10\x00\x00\x00"  # pixel-format
+        b"\x00\x00\x00\x00"  # server-name-len
+    )
+
     def setUp(self) -> None:
         self.client = client.VNCDoToolClient()
         self.client.transport = mock.Mock()
@@ -18,10 +26,9 @@ class TestVNCDoToolClient(TestCase):
 
     def test_vncConnectionMade(self):
         cli = self.client
-        cli._packet = bytearray(b"RFB 003.003\n")
+        cli._packet = bytearray(self.MSG_HANDSHAKE)
         cli._handleInitial()
-        cli._handleServerInit(b" " * 24)
-        cli.vncConnectionMade()
+        cli._handleServerInit(self.MSG_INIT)
         factory = cli.factory
         factory.clientConnectionMade.assert_called_once_with(cli)
         self.client.setEncodings.assert_called_once_with([
@@ -53,9 +60,9 @@ class TestVNCDoToolClient(TestCase):
     @mock.patch('vncdotool.client.Deferred')
     def test_captureScreen(self, Deferred):
         cli = self.client
-        cli._packet = bytearray(b"RFB 003.003\n")
+        cli._packet = bytearray(self.MSG_HANDSHAKE)
         cli._handleInitial()
-        cli._handleServerInit(b" " * 24)
+        cli._handleServerInit(self.MSG_INIT)
         cli.vncConnectionMade()
         fname = 'foo.png'
 
@@ -75,9 +82,9 @@ class TestVNCDoToolClient(TestCase):
     @mock.patch('vncdotool.client.Deferred')
     def test_expectScreen(self, Deferred, image_open):
         cli = self.client
-        cli._packet = bytearray(b"RFB 003.003\n")
+        cli._packet = bytearray(self.MSG_HANDSHAKE)
         cli._handleInitial()
-        cli._handleServerInit(b" " * 24)
+        cli._handleServerInit(self.MSG_INIT)
         cli.vncConnectionMade()
         fname = 'something.png'
 

vncdotool/loggingproxy.py
@@ -11,7 +11,7 @@ from twisted.protocols import portforward
 from twisted.python.failure import Failure
 
 from .client import KEYMAP, VNCDoToolClient
-from .rfb import AuthTypes, IntEnumLookup, Rect
+from .rfb import AuthTypes, Encoding, IntEnumLookup, PixelFormat, Rect
 
 log = logging.getLogger(__name__)
 
@@ -119,7 +119,7 @@ class RFBServer(Protocol):  # type: ignore[misc]
 
     def _handle_clientInit(self) -> None:
         shared = self.buffer[0]
-        log.debug("Shared server: %s", shared)
+        log.debug("Client shares: %s", shared)
         del self.buffer[:1]
         # XXX react to shared
         # XXX send serverInit
@@ -135,13 +135,16 @@ class RFBServer(Protocol):  # type: ignore[misc]
         block = bytes(self.buffer[1:nbytes])
         del self.buffer[:nbytes]
         if ptype == MsgC2S.SET_PIXEL_FORMAT:
-            args = unpack('!xxxBBBBHHHBBBxxx', block)
-            self.handle_setPixelFormat(*args)
+            (args,) = unpack('!xxx16s', block)
+            pixel_fomat = PixelFormat.from_bytes(args)
+            self.handle_setPixelFormat(pixel_fomat)
         elif ptype == MsgC2S.SET_ENCODING:
             nencodings, = unpack('!xH', block)
             nbytes = 4 * nencodings
             encodings = unpack_from('!' + 'I' * nencodings, self.buffer)
             del self.buffer[:nbytes]
+            for encoding in encodings:
+                log.debug(f"Client announces {Encoding.lookup(encoding)!r}")
             self.handle_setEncodings(encodings)
         elif ptype == MsgC2S.FRAMEBUFFER_UPDATE_REQUEST:
             inc, x, y, w, h = unpack('!BHHHH', block)
@@ -171,7 +174,7 @@ class RFBServer(Protocol):  # type: ignore[misc]
         self.handle_keyEventExtended(keysym, down_flag, keycode)
         self._handler = self._handle_protocol, 1
 
-    def handle_setPixelFormat(self, bbp: int, depth: int, bigendian: bool, truecolor: bool, rmax: int, gmax: int, bmax: int, rshift: int, gshift: int, bshift: int) -> None:
+    def handle_setPixelFormat(self, pixel_format: PixelFormat) -> None:
         pass
 
     def handle_setEncodings(self, encodings: Sequence[int]) -> None:
@@ -203,6 +206,9 @@ class NullTransport:
         return
 
     def setTcpNoDelay(self, enabled: bool) -> None:
+        return
+
+    def loseConnection(self) -> None:
         return
 
 

commit e77a236e28263256f539ab5c7123ab4d1747cf77

Regressions in 1.1.0pre (#247)
* fix(client): PILLOW raw image names

The RAW image formats using `int`s are named differently:

> Traceback (most recent call last):
>   File "vncdotool/rfb.py", line 1117, in dataReceived
>     self._handler()
>   File "vncdotool/rfb.py", line 1126, in _handleExpected
>     self._expected_handler(block, *self._expected_args, **self._expected_kwargs)
>   File "vncdotool/rfb.py", line 708, in _handleDecodeRAW
>     self.updateRectangle(x, y, width, height, block)
>   File "vncdotool/client.py", line 425, in updateRectangle
>     update = Image.frombytes('RGB', size, data, 'raw', self.image_mode)
>   File "PIL/Image.py", line 2882, in frombytes
>     im.frombytes(data, decoder_name, args)
>   File "PIL/Image.py", line 810, in frombytes
>     d = _getdecoder(self.mode, decoder_name, args)
>   File "PIL/Image.py", line 403, in _getdecoder
>     return decoder(mode, *args + extra)
> builtins.ValueError: unknown raw mode for given image mode

See source code of PILLOW for complete list:
<https://github.com/python-pillow/Pillow/blob/main/src/libImaging/Unpack.c>

Fixes: 15d1f70115ef4b30a3f089149d8812ee8eb2ce36

* fix(qemu): Fix Qemu extended key handling again

The PSEUDO_QEMU_EXTENDED_KEY_EVENT encoding may be part of a larger
set of update rectangles. Continue parsing the following rectangles
instead of going back to connection handling.

Actually call `_doConnection()` to handle either the next rectangle in
row or return to `_handleConnection` if it was the last rectangle.

Only call callback `commitUpdate()` if at least one real update
happened. Otherwise a PSEUDO_QEMU_EXTENDED_KEY_EVENT will trigger the
Deferred and `capture` will fail, as no real update has happened.

Fixes: 53627d814f97c5d14fd2cc9b879c4b967058b4cc

* fix(command): rms as float

vncdotool/client.py
@@ -141,8 +141,8 @@ PF2IM = {
     RGB24: "RGB",
     RGB32: "RGBX",
     BGR16: "BGR;16",
-    rfb.PixelFormat(24, 24, False, True, 255, 255, 255, 16, 8, 0): "BGR;24",
-    rfb.PixelFormat(32, 24, False, True, 255, 255, 255, 16, 8, 0): "BGR;32",
+    rfb.PixelFormat(24, 24, False, True, 255, 255, 255, 16, 8, 0): "BGR",
+    rfb.PixelFormat(32, 24, False, True, 255, 255, 255, 16, 8, 0): "BGRX",
 }
 
 
@@ -321,7 +321,7 @@ class VNCDoToolClient(rfb.RFBClient):
                 sum_ = sum((h - e) ** 2 for h, e in zip(hist, self.expected))
                 rms = math.sqrt(sum_ / len(hist))
 
-                log.debug('rms:%d maxrms:%d', rms, maxrms)
+                log.debug('rms:%f maxrms:%f', rms, maxrms)
                 if rms <= maxrms:
                     return self
 

vncdotool/command.py
@@ -183,7 +183,7 @@ def build_command_list(
             factory.deferred.addCallback(client.captureScreen, filename, int(incremental_refreshes))
         elif cmd == 'expect':
             filename = args.pop(0)
-            rms = int(args.pop(0))
+            rms = float(args.pop(0))
             factory.deferred.addCallback(client.expectScreen, filename, rms)
         elif cmd == 'rcapture':
             filename = args.pop(0)
@@ -199,7 +199,7 @@ def build_command_list(
             filename = args.pop(0)
             x = int(args.pop(0))
             y = int(args.pop(0))
-            rms = int(args.pop(0))
+            rms = float(args.pop(0))
             factory.deferred.addCallback(client.expectRegion, filename, x, y, rms)
         elif cmd in ('pause', 'sleep'):
             duration = float(args.pop(0)) / warp

vncdotool/rfb.py
@@ -659,7 +659,8 @@ class RFBClient(Protocol):  # type: ignore[misc]
         if self.rectangles:
             self.expect(self._handleRectangle, 12)
         else:
-            self.commitUpdate(self.rectanglePos)
+            if self.rectanglePos:
+                self.commitUpdate(self.rectanglePos)
             self.expect(self._handleConnection, 1)
 
     def _handleRectangle(self, block: bytes) -> None:
@@ -691,6 +692,8 @@ class RFBClient(Protocol):  # type: ignore[misc]
                 self._handleDecodeDesktopSize(width, height)
             elif encoding == Encoding.PSEUDO_QEMU_EXTENDED_KEY_EVENT:
                 self.negotiated_encodings.add(Encoding.PSEUDO_QEMU_EXTENDED_KEY_EVENT)
+                del self.rectanglePos[-1]  # undo append as this is no real update
+                self._doConnection()
             else:
                 log.msg(f"unknown encoding received {Encoding.lookup(encoding)!r}")
                 self.transport.loseConnection()